7aa15e5006f77811e1e7168801f353d5ecb8987e
Dylan Wolf <100877190+dr-boogie-o-wonder@users.noreply.github.com>
2022-03-06 10:16:10 -0500

    Creates charts and saves them as .html files

--- chart.py
+++ chart.py
@@ -6,11 +6,11 @@ def create_chart():
     df = pd.read_csv('data/weapons_index.csv')
     reversed_df = df.iloc[::-1]
     fig = px.line(reversed_df, x="Date", y="Adj Close", title='Defense Contractors Direct Index')
-    fig.show()
+    fig.write_html("defense_contractors.html", auto_open=True)
     reversed_reddit_df = pd.read_csv('data/mentions_of_war.csv')
     reddit_df = reversed_reddit_df.iloc[:-1]
-    fig = px.line(reddit_df, x="Date", y="Mentions of \'War\'", title='Mentions of War on r/News')
-    fig.show()
+    fig2 = px.line(reddit_df, x="Date", y="Mentions of \'War\'", title='Mentions of War on r/News')
+    fig2.write_html("mentions_of_war.html", auto_open=True)
 
 
 create_chart()
